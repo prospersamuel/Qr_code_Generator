e6953560a8ea40ac043c2a9db3e05035b57fa1b9
Prosper Samuel <prospersamuel100@gmail.com>
2024-11-15 15:15:13 +0100

    qr code generator

---
+++ qr.js
@@ -0,0 +1,26 @@
+    let button = document.querySelector("button")
+    let imgBox = document.getElementById("imgBox")
+    let qrImage = document.getElementById("qrimage")
+    let qrText = document.getElementById("qrtext")
+
+
+
+    function generateQR() {
+        if(qrText.value.length > 0){
+            qrImage.src = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + qrText.value;
+            imgBox.classList.add("show-img")
+
+        }
+        else{
+            qrText.classList.add('error')
+            setTimeout(() => {
+                qrText.classList.remove('error')
+
+            },10000)
+        }
+
+    }
+
+
+
+    button.addEventListener("click", generateQR)
